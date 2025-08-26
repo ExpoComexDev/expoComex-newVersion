0d8973a3c4161499a00af93b4185b7136b351a8b
ExpoComexDev <dgproveedores4@hotmail.com>
2025-08-26 18:02:09 -0300

    ECN-1 Primer commit, base web + modificacion de algunos textos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LandingPage from './components/LandingPage'
 
 function App() {
 
   return (
     <>
-      <h1>EXPO COMEX</h1>
+      <LandingPage />
     </>
   )
 }
